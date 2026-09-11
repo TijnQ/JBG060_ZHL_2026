267f1a5376425a8fe0b74f8f131250f0a1ba9560
Tijn Quivooij <t.j.quivooij@student.tue.nl>
2026-09-11 11:29:35 +0200

    feat: added outlines states and counties and made empty values transparant

--- EDA_farmland/plot_map.py
+++ EDA_farmland/plot_map.py
@@ -1,6 +1,9 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import rioxarray as rxr
+from matplotlib.lines import Line2D
+
+from processing_data.loading_impact_data import load_admin_boundaries
 
 
 def plot_map(farm_path, label_farm, admin_path, output):
@@ -12,7 +15,10 @@ def plot_map(farm_path, label_farm, admin_path, output):
     farm_data = rxr.open_rasterio(farm_path, masked=True)
 
     #show farm data only over ssd
-    farm_ssd = farm_data.rio.clip(basemap.geometry, basemap.crs, drop=True)
+    farm_ssd = farm_data.rio.clip(basemap.geometry, basemap.crs, drop=True, from_disk=True)
+
+    #make nodata values transparant
+    farm_ssd = farm_ssd.where(farm_ssd > 0)
 
     _, ax = plt.subplots(figsize=(12,8))
 
@@ -25,7 +31,7 @@ def plot_map(farm_path, label_farm, admin_path, output):
     else:
         vmax = calc_vmax
 
-
+    #plot heatmap
     farm_ssd[0].plot(
         ax=ax,
         cmap='YlOrRd',
@@ -35,6 +41,28 @@ def plot_map(farm_path, label_farm, admin_path, output):
     )
 
     basemap.plot(ax=ax, facecolor='none', edgecolor='black', linewidth=1)
+
+
+    admin1, admin2 = load_admin_boundaries()
+
+    #Nothern Bahr el Ghazal
+    nbeg_state = admin1[admin1['adm1_name'].str.contains('Northern Bahr el Ghazal', case=False, na=False)]
+
+    #River lol, Aweil
+    rla_counties = admin2[admin2['adm2_name'].str.contains('Aweil|Lol', case=False, na=False)]
+
+
+    #plot oultines ontop
+    nbeg_state.plot(ax=ax, facecolor='none', edgecolor='cyan', linewidth=3)
+    rla_counties.plot(ax=ax, facecolor='none', edgecolor='blue', linewidth=1.5)
+
+
+
+    legend_elements = [
+        Line2D([0], [0], color='cyan', lw=3, label='Northern Bahr el Ghazal State'),
+        Line2D([0], [0], color='blue', lw=2, linestyle='--', label='Aweil Counties'),
+    ]
+    ax.legend(handles=legend_elements, loc='upper left', frameon=True, facecolor='white', framealpha=0.9, fontsize=10)
 
     ax.set_axis_off()
     plt.title(f" {label_farm} Distribution in South Sudan", fontsize=16, pad=15)
